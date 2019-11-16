2068ae760e7f9345189fa633c16e0a5872365561
winkhai <kwin.2011@gmail.com>
2019-11-16 14:37:46 -0500

    add pythonfiles for training

---
+++ train/img_aug.py
@@ -0,0 +1,20 @@
+from torchvision import datasets, transforms
+
+
+std_norm = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
+
+img_transforms = {'train': transforms.Compose([transforms.RandomResizedCrop(size=256),
+                                               transforms.RandomHorizontalFlip(),
+                                               transforms.RandomRotation(degrees=30),
+                                               transforms.CenterCrop(size=224),
+                                               transforms.ToTensor(),
+                                               std_norm]),
+                   'valid': transforms.Compose([transforms.Resize(size=256),
+                                                transforms.CenterCrop(size=224),
+                                                transforms.ToTensor(),
+                                                std_norm]),
+                   'test': transforms.Compose([transforms.Resize(size=256),
+                                               transforms.CenterCrop(size=224),
+                                               transforms.ToTensor(),
+                                               std_norm])
---
+++ train/img_dataloader.py
@@ -0,0 +1,43 @@
+import torch
+from torchvision import datasets, transforms
+
+train_data_path = "data/train/"
+train_data = torchvision.datasets.ImageFolder(root=train_data_path,transform=img_transforms['train'])
+
+valid_data_path = "data/valid/"
+valid_data = torchvision.datasets.ImageFolder(root=valid_data_path,transform=img_transforms['valid'])
+
+test_data_path = "data/test/"
+test_data = torchvision.datasets.ImageFolder(root=test_data_path,transform=img_transforms['test'])
+
+print(f'there are {len(train_data.classes)} classes in the training dataset.')
+print(f'there are {len(valid_data.classes)} classes in the validation dataset.')
+print(f'there are {len(test_data.classes)} classes in the testing dataset.')
+
+
+#Params
+batch_size = 64
+num_workers = 0
+
+train_loader = torch.utils.data.DataLoader(train_data,
+                                           batch_size=batch_size,
+                                           num_workers=num_workers,
+                                           shuffle=True)
+valid_loader = torch.utils.data.DataLoader(valid_data,
+                                           batch_size=batch_size,
+                                           num_workers=num_workers,
+                                           shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data,
+                                           batch_size=batch_size,
+                                           num_workers=num_workers,
+                                           shuffle=False)
+
+                                           loaders = {
+                                               'train': train_loader,
+                                               'valid': valid_loader,
+                                               'test': test_loader
+                                           }
+
+print(f'Num. of Images in Training Set: {len(train_loader.dataset)}')
+print(f'Num. of Images in Validation Set: {len(valid_loader.dataset)}')
+print(f'Num. of Images in Testing Set: {len(test_loader.dataset)}')
